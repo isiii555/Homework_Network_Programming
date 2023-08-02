24dc8832626e635d3b17cf17fee8379a7df340fb
İslam Salamzadə <islam.salamzade.191@gmail.com>
2023-08-02 17:26:51 +0400

    ScreenshotApp

---
+++ Screenshot App/Screenshot App/MainWindow.xaml.cs
@@ -0,0 +1,119 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Screenshot_App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public BitmapImage PhotoSource
+        {
+            get { return (BitmapImage)GetValue(PhotoSourceProperty); }
+            set { SetValue(PhotoSourceProperty, value); }
+        }
+
+        public Dispatcher Dispatcher { get; set; } = Dispatcher.CurrentDispatcher;
+
+        // Using a DependencyProperty as the backing store for PhotoSource.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PhotoSourceProperty =
+            DependencyProperty.Register("PhotoSource", typeof(BitmapImage), typeof(MainWindow));
+        public IPAddress IpAdress { get; set; }
+
+        public IPEndPoint IpAdressEndPoint { get; set; }
+
+        public TcpClient Client { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            PhotoSource = new();
+            DataContext = this;
+            IpAdress = IPAddress.Parse("192.168.100.225");
+            IpAdressEndPoint = new(IpAdress, 27001);
+            Client = new TcpClient();
+        }
+
+        public RelayCommand StartCommand
+        {
+            get => new RelayCommand(() =>
+            {
+                Task.Run(() =>
+                {
+                    while (true)
+                    {
+                        try
+                        {
+                            Client = new();
+                            Client.Connect(IpAdressEndPoint);
+                            if (Client.Connected)
+                            {
+                                try
+                                {
+                                    using (NetworkStream networkStream = Client.GetStream())
+                                    {
+                                        byte[] imageData = new byte[4096];
+                                        int bytesRead;
+                                        using (MemoryStream memoryStream = new MemoryStream())
+                                        {
+
+                                            while ((bytesRead = networkStream.Read(imageData, 0, imageData.Length)) > 0)
+                                            {
+                                                memoryStream.Write(imageData, 0, bytesRead);
+                                            }
+
+
+                                            memoryStream.Seek(0, SeekOrigin.Begin);
+                                            BitmapImage bitmapImage = new BitmapImage();
+                                            bitmapImage.BeginInit();
+                                            bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                                            bitmapImage.StreamSource = memoryStream;
+                                            bitmapImage.EndInit();
+                                            bitmapImage.Freeze();
+
+                                            Dispatcher.Invoke(() => PhotoSource = bitmapImage);
+                                        }
+                                    }
+                                }
+                                catch (Exception ex)
+                                {
+                                    // Handle exceptions if any
+                                    MessageBox.Show("Error loading the image: " + ex.Message);
+                                }
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                        }
+                        Task.Delay(5000).Wait();
+                    }
+                }
+                );
+            })
+            {
+            };
+        }
+    }
+}
---
+++ Screenshot App/ScreenshotSender/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Drawing;
+using System.Drawing.Imaging;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+IPAddress iPAddress = IPAddress.Parse("192.168.100.225");
+IPEndPoint iPEndPoint = new(iPAddress, 27001);
+
+TcpListener listener = new(iPEndPoint);
+
+listener.Start();
+
+Console.WriteLine("Listening...");
+
+while (true)
+{
+    var client = listener.AcceptTcpClient();
+    Console.WriteLine($"{client.Client.LocalEndPoint} connected");
+    Task.Run(() =>
+    {
+
+        while (true)
+        {
+            var stream = client.GetStream();
+            var bitmap = ScreenShot();
+            ImageConverter converter = new ImageConverter();
+            var bytes = (byte[])converter.ConvertTo(bitmap, typeof(byte[]))!;
+            stream.Write(bytes);
+            Console.WriteLine("Screenshoted!");
+
+            //Yoxlama
+
+            Console.WriteLine($"{bytes.Length} Sent");
+            stream.Close();
+        }
+    });
+}
+
+Bitmap ScreenShot()
+{
+    Bitmap memoryImage;
+    memoryImage = new Bitmap(1920,1080);
+    Size s = new Size(memoryImage.Width, memoryImage.Height);
+
+    Graphics memoryGraphics = Graphics.FromImage(memoryImage);
+
+    memoryGraphics.CopyFromScreen(0, 0, 0, 0, s);
+
+    return memoryImage;
+}
